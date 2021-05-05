5480f119d981d97103e52f7110c8e28a82c91bb4
Antonin <antoninlefevre45@icloud.com>
2021-05-05 12:05:13 +0200

    Update manimce.py

--- manimce.py
+++ manimce.py
@@ -870,6 +870,88 @@ class Gradient_descent_3D(ThreeDScene):
         self.stop_ambient_camera_rotation()
         self.wait()
 
+## Animation surfaces
+class SurfacesAnimation(ThreeDScene):  ####### Surface
+    def construct(self):
+        axes = ThreeDAxes()
+        cylinder = ParametricSurface(
+            lambda u, v: np.array([
+                np.cos(TAU * v),
+                np.sin(TAU * v),
+                2 * (1 - u)
+            ]),
+            resolution=(6, 32)).fade(0.5)  # Resolution of the surfaces
+
+        paraboloid = ParametricSurface(
+            lambda u, v: np.array([
+                np.cos(v) * u,
+                np.sin(v) * u,
+                u ** 2
+            ]), v_max=TAU,
+            checkerboard_colors=[PURPLE_D, PURPLE_E],
+            resolution=(10, 32)).scale(2)
+
+        para_hyp = ParametricSurface(
+            lambda u, v: np.array([
+                u,
+                v,
+                u ** 2 - v ** 2
+            ]), v_min=-2, v_max=2, u_min=-2, u_max=2, checkerboard_colors=[BLUE_D, BLUE_E],
+            resolution=(15, 32)).scale(1)
+
+        cone = ParametricSurface(
+            lambda u, v: np.array([
+                u * np.cos(v),
+                u * np.sin(v),
+                u
+            ]), v_min=0, v_max=TAU, u_min=-2, u_max=2, checkerboard_colors=[GREEN_D, GREEN_E],
+            resolution=(15, 32)).scale(1)
+
+        hip_one_side = ParametricSurface(
+            lambda u, v: np.array([
+                np.cosh(u) * np.cos(v),
+                np.cosh(u) * np.sin(v),
+                np.sinh(u)
+            ]), v_min=0, v_max=TAU, u_min=-2, u_max=2, checkerboard_colors=[YELLOW_D, YELLOW_E],
+            resolution=(15, 32))
+
+        ellipsoid = ParametricSurface(
+            lambda u, v: np.array([
+                1 * np.cos(u) * np.cos(v),
+                2 * np.cos(u) * np.sin(v),
+                0.5 * np.sin(u)
+            ]), v_min=0, v_max=TAU, u_min=-PI / 2, u_max=PI / 2, checkerboard_colors=[TEAL_D, TEAL_E],
+            resolution=(15, 32)).scale(2)
+
+        sphere = ParametricSurface(
+            lambda u, v: np.array([
+                1.5 * np.cos(u) * np.cos(v),
+                1.5 * np.cos(u) * np.sin(v),
+                1.5 * np.sin(u)
+            ]), v_min=0, v_max=TAU, u_min=-PI / 2, u_max=PI / 2, checkerboard_colors=[RED_D, RED_E],
+            resolution=(15, 32)).scale(2)
+
+        self.set_camera_orientation(phi=75 * DEGREES)
+        self.begin_ambient_camera_rotation(rate=0.2)
+
+        self.add(axes)
+        self.play(Write(sphere))
+        self.wait()
+        self.play(ReplacementTransform(sphere, ellipsoid))
+        self.wait()
+        self.play(ReplacementTransform(ellipsoid, cone))
+        self.wait()
+        self.play(ReplacementTransform(cone, hip_one_side))
+        self.wait()
+        self.play(ReplacementTransform(hip_one_side, para_hyp))
+        self.wait()
+        self.play(ReplacementTransform(para_hyp, paraboloid))
+        self.wait()
+        self.play(ReplacementTransform(paraboloid, cylinder))
+        self.wait()
+        self.play(FadeOut(cylinder))
+
+
 # SVG
 
 ## stickman
